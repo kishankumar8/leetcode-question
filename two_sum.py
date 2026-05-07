9ad636858211a7d8c8a1661bf9cb6c27f8929200
kishan kumar <kt2561060@gmail.com>
2026-05-07 20:53:28 +0530

    Fix twoSum method implementation and variable names

--- two_sum.py
+++ two_sum.py
@@ -1,16 +1,9 @@
 class Solution(object):
-    def twoSum(self, num, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap={}
-        for i in range(len(num)):
-            complement=target-num[i]
-
-            if complement in hashmap:
-                return
-
-                [hashmap[complement],i]
-                hashmap[nums[i]]=i
+    def twoSum(self, nums, target):
+         hashmap={}
+         for i in range(len(nums)):
+                compliment=target-nums[i]
+                
+                if compliment in hashmap:
+                    return [hashmap[compliment], i]
+                hashmap[nums[i]] = i
